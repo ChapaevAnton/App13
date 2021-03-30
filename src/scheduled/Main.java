a31f17734e4991b7fd69990aab8aea18fd574bcb
ChapaevAnton <virsus2009@gmail.com>
2021-03-30 10:27:39 +0700

    13.7
    *Дополнительное задание

---
+++ src/scheduled/Main.java
@@ -0,0 +1,12 @@
+package scheduled;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Task task = new Task();
+            task.performTask(20);
+    }
+
+}
---
+++ src/scheduled/Task.java
@@ -0,0 +1,27 @@
+package scheduled;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+class Task {
+
+    private final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
+
+    public void performTask(int delayStop) {
+        Runnable beeper = () -> System.out.println("Run->" + Thread.currentThread().getName());
+
+        ScheduledFuture<?> beeperHandle =
+                scheduler.scheduleAtFixedRate(beeper, 4, 4, TimeUnit.SECONDS);
+
+        Runnable canceller = () -> {
+            beeperHandle.cancel(false);
+            System.out.println("Stop");
+        };
+
+        scheduler.schedule(canceller, delayStop, TimeUnit.SECONDS);
+
+    }
+}
